0e53aa7abfe244c7ade93b705e6aa8ede3fc9af4
MahmoudAlsameen <121268006+MahmoudAlsameen@users.noreply.github.com>
2023-10-17 10:48:43 +0300

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import userValidation from '../middleware/validation.js'
 import auth from '../middleware/auth.js'
 import {signUpValidationSchema,signInValidationSchema} from '../validation/user.validation.js'
-import {signup, login, updateuser, hdeleteuser, sdeleteuser, logout} from '../controllers/user.controller.js'
+import {signup, login, updateuser, hdeleteuser, sdeleteuser, logout, loginGoogle} from '../controllers/user.controller.js'
 
 
 
@@ -11,6 +11,7 @@ const userRoutes = express.Router()
 
 userRoutes.post("/signup",userValidation(signUpValidationSchema,'body'),signup)
 userRoutes.post("/login",userValidation(signInValidationSchema,'body'),login)
+userRoutes.post("/loginGoogle",loginGoogle)
 userRoutes.put("/updateuser",auth,updateuser)
 userRoutes.delete("/hdeleteuser",auth,hdeleteuser)
 userRoutes.put("/sdeleteuser",auth,sdeleteuser)
