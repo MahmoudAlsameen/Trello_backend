f99c03b08ce4ae73a3a4d4b8acbdbf7e8569466b
MahmoudAlsameen <121268006+MahmoudAlsameen@users.noreply.github.com>
2023-10-19 11:44:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-import express from 'express'
-import connection from './db/connection.js'
-import userRoutes from './routes/user.routes.js'
-import taskRoutes from './routes/task.routes.js'
-
+import express from 'express';
+import connection from './db/connection.js';
+import userRoutes from './routes/user.routes.js';
+import taskRoutes from './routes/task.routes.js';
+import cors from 'cors'; // Import the cors middleware
 
 const app = express();
-const port=process.env.PORT || 3000;
-app.use(express.json())
+const port = 3000;
+app.use(express.json());
 
+connection();
 
-connection()
+// Use the cors middleware to enable CORS and allow requests from 'http://localhost:5173'
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend's URL
+  methods: 'GET,POST,PUT,DELETE',
+}));
 
-app.use(userRoutes)
-app.use(taskRoutes)
+app.use(userRoutes);
+app.use(taskRoutes);
 
-app.get('/',(req,res)=>{
-    console.log('Home page')
-    res.send('Home page')})
+app.get('/', (req, res) => {
+  console.log('Home page');
+  res.send('Home page');
+});
 
-
-    app.listen(port,() => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
