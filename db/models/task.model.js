cd92713c480eb566353218678dcfe56080d39c6d
MahmoudAlsameen <121268006+MahmoudAlsameen@users.noreply.github.com>
2023-10-30 02:54:24 +0200

    Update task.model.js

--- db/models/task.model.js
+++ db/models/task.model.js
@@ -4,6 +4,12 @@ const taskSchema = new mongoose.Schema({
   title: String,
   description: String,
   status: { type: String, enum: ['toDo', 'doing', 'done'] },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
   creatorID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   deadline: Date,
